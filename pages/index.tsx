77428849e0b07c7a5597f8382c1723f0ec881821
Muhammad Hasaan <114328181+mhmAdmin1122@users.noreply.github.com>
2024-05-20 18:36:17 +0000

    few more changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
 import React from "react";
-import logo from "@/public/logo.jpg";
 
 const Home = () => {
   return (
     <div className="message-box flex justify-center items-center h-screen">
       <div className="message-body shadow-xl shadow-gray-700 w-[500px] flex flex-col items-center gap-5 bg-gray-100 px-[20px] py-[40px] rounded-lg">
         <div className="flex items-center gap-3 flex-wrap">
-          <Image
-            src={logo}
-            alt="logo-pic"
-            className="w-[90px] h-[60px] logoPic"
-          />
+         
           <div className="flex flex-col gap-2 leading-4">
             <h1 className="font-bold text-3xl text-[#000]">Rise Marketing</h1>
             <p className="font-normal text-[20px] text-gray-400">
